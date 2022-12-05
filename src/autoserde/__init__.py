c368887342eb6fece2540e33fe30746c4cd27450
limoiie <limo.iie4@gmail.com>
2022-12-05 17:48:09 +0800

    chore: Fix missing package __version__

--- src/autoserde/__init__.py
+++ src/autoserde/__init__.py
@@ -1,13 +1,23 @@
 """A lightweight serde framework."""
 
-from autodict import dictable, from_dictable, to_dictable
-
-from autoserde.autoserde import AutoSerde, NotDeserializable, NotSerializable, \
-    Serdeable
-from autoserde.formats.base import SerdeFormat
-
 __all__ = ['serdeable', 'serializable', 'deserializable', 'AutoSerde',
            'NotSerializable', 'NotDeserializable', 'Serdeable', 'SerdeFormat']
+
+try:
+    import importlib.metadata as _importlib_metadata
+except ModuleNotFoundError:
+    # noinspection PyUnresolvedReferences
+    import importlib_metadata as _importlib_metadata
+
+try:
+    __version__ = _importlib_metadata.version("autoserde")
+except _importlib_metadata.PackageNotFoundError:
+    __version__ = "unknown version"
+
+from autodict import dictable, from_dictable, to_dictable
+
+from .autoserde import AutoSerde, NotDeserializable, NotSerializable, Serdeable
+from .formats.base import SerdeFormat
 
 serdeable = dictable
 
